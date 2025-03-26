9cfae8c530bcdc10c1574fd351840e06fd597d79
sonvh <hoaison95.ftu@gmail.com>
2025-03-26 16:21:28 +0700

    Remove dotenv

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,6 +1,3 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
-
 export default {
   client: 'pg',
   connection: process.env.DATABASE_URL,
--- server/src/modules/auth/index.ts
+++ server/src/modules/auth/index.ts
@@ -1,9 +1,6 @@
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import { db } from '../../db';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 // Define a custom user interface
 export interface UserProfile {
